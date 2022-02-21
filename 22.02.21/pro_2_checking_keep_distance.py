632cc887c9109b376169ef7e4b3f92e49f23f3d6
soeun lee <thdmsdl87@gmail.com>
2022-02-21 22:23:28 +0900

    22.02.21 문제 푸는데 걸린 시간 추가

--- 22.02.21/pro_2_checking_keep_distance.py
+++ 22.02.21/pro_2_checking_keep_distance.py
@@ -2,6 +2,7 @@
 distance==2면 check해야하지만
 distance==1면 그냥 바로 False => check를 하게 되면, 당연히 PP 이런식으로 붙은거라 p와p 사이에 O, P 이런게 없어서 check 통과해버림
 매우 주의하기
+*힌트 봤고, 정답까지 46분 걸림
 """
 def solution(places):
     lst=[]
