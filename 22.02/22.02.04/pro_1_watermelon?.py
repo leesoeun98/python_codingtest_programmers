b4523e7f329502baabfa42ed3d71fb4e13549a9a
이소은 <52341650+leesoeun98@users.noreply.github.com>
2022-04-04 12:36:12 +0900

    Delete pro_1_watermelon?.py

--- 22.02/22.02.04/pro_1_watermelon?.py
+++
@@ -1,9 +0,0 @@
-"""def solution(n):
-    if n==1:
-        return "수"
-    elif n%2==0:
-        return "수박"*(n//2)
-    else:
-        return "수박"*(n//2)+"수" """
-def solution(n):
-    return "수박"*(n//2)+"수"(n%2)
