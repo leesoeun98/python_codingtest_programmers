64767014591aa9e167497c74ca1cdc9497e64dab
soeun lee <thdmsdl87@gmail.com>
2023-05-28 00:17:33 +0900

    [개선] 23.05.27 프로그래머스 레벨 1 : 1문제 (둘만의 암호)
    접근법 : skip에 있는 문자열을 미리 빼고 list만들어서 바로 index에 +만 하는 식으로 개선

--- 23.05/05.27/Pro_1/둘만의 암호.py
+++ 23.05/05.27/Pro_1/둘만의 암호.py
@@ -1,16 +1,11 @@
 # 19분 소요, 혼자 풂 (3차 시도에서 성공) / 테스트케이스는 남의 거 참고
 # index out of range에 주의하고, 코드 신중히 짜자 +1, %26 같은거 안해서 계속 오류남
-alphabetList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-                "v", "w", "x", "y", "z"]
+# 어차피 skip에 있는 문자열은 계속 포함이 안되니까 빼고 시작하자
 
 
 def solution(s, skip, index):
+    alphabetList = [chr(a) for a in range(ord('a'), ord('z')+1) if chr(a) not in skip]
     answer = []
     for letter in s:
-        count, targetIndex = 0, alphabetList.index(letter)
-        while count != index:
-            if alphabetList[(targetIndex + 1) % 26] not in skip:
-                count += 1
-            targetIndex += 1
-        answer.append(alphabetList[targetIndex % 26])
+        answer.append(alphabetList[(alphabetList.index(letter)+index) % len(alphabetList)])
     return ''.join(answer)
