d7ff6baa2401b9214fd17439784a249fcf6990cf
soeun lee <thdmsdl87@gmail.com>
2023-05-28 01:02:18 +0900

    [해결, 개선] : 23.05.27 프로그래머스 레벨 1 : 1문제
    find, join 등이 시간초과나서 index로 찾는 것과, del 사용해서 시간초과 해결

--- 23.05/05.27/Pro_1/햄버거 만들기.py
+++ 23.05/05.27/Pro_1/햄버거 만들기.py
@@ -1,13 +1,22 @@
 # 24:18 -
 # 1231 이 stack에 순서대로 쌓여있을때만 햄버거 만듦 => list가 주어질때 만들 수 있는 햄버거 개수 int return
 # 1차 시도 : 13분 소요, 혼자 풂 (정답인 것 같으나 시간 초과)
+
+# 2차 시도 : find, join이 애초에 시간 초과 (30분 소요, 디른 사람 풀이 보고 이해함)
+# => list에서 1일때만 참색 시작 (바로 햄버거가 되면 idx가 증가하는 거고, 햄버거가 안되면 안에서 찾은 후 해당 4개 원소를 del 한 후 idx 안에서 다시 탐색)
+# => 이렇게 하면 무한대로 되돌아오지 않아도, 한 방향으로만 탐색 끝낼 수 있음 (len(ingredient)-4 < idx 면 종료)
 def solution(ingredient):
-    answer, ingredient_s = 0, ''.join(map(str, ingredient))
-    while True:
-        idx = ingredient_s.find("1231")
-        if idx == - 1:
-            break
-        else:
-            answer += 1
-            ingredient_s = ingredient_s[:idx] + ingredient_s[idx+4:]
+    answer, idx = 0, 0
+    while idx < len(ingredient)-3:
+        # 처음이 빵일때만 탐색 시작
+        if ingredient[idx] == 1:
+            # 빵 시작일때 해당 범위 내에서 1,2,3,1 있으면 계속 찾기 (continue, idx 조정)
+            # 한 번 찾으면, 해당 4개 원소 지우고 + 1,2,3,1 지웠는데 앞에 1이 있었으면 햄버거 또 만들 수 있으니까 idx-3
+            if ingredient[idx:idx+4] == [1,2,3,1]:
+                del ingredient[idx:idx+4]
+                idx -= 3
+                answer += 1
+                continue
+        idx += 1
+
     return answer
