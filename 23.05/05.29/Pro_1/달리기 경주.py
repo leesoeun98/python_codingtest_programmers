32419658cd1943da7a62e73bd7e324d0f743060f
soeun lee <thdmsdl87@gmail.com>
2023-05-29 13:56:04 +0900

    [미해결] 23.05.29 프로그래머스 레벨 1 : 1문제 (달리기 경주)

--- 23.05/05.29/Pro_1/달리기 경주.py
+++ 23.05/05.29/Pro_1/달리기 경주.py
@@ -1,9 +1,19 @@
 # 13:32 -
+# 1차 시도 : 배열에 있는 원소로 다 swap하기 (정렬 문제) => 시간 초과
+# 2차 시도 : 이름 별 idx (hash 사용)
+
 # 바로 앞 선수 추월 시, 추월한 사람 이름 부름
 # 현 순서 이름 배열, 이름 부른 배열 줄 때, 선수 등수 순 이름 배열 반환하기
+result = dict()
+
+
 def solution(players, callings):
+    for idx, player in enumerate(players):
+        result[player] = idx
+
     for calling in callings:
-        idx = players.index(calling)
-        players[idx] = players[idx-1]
-        players[idx-1]=calling
-    return players
+        target = [name for name, rank in result.items() if rank == result[calling] - 1][0]
+        result[calling] -= 1
+        result[target] += 1
+    answer = dict(sorted(result.items(), key=lambda item: item[1]))
+    return list(answer.keys())
