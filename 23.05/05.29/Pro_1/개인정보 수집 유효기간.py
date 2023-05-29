6f1768ffb399fe2b0de424c920dbc76487013aaf
soeun lee <thdmsdl87@gmail.com>
2023-05-29 15:01:26 +0900

    [해결, 개선] 23.05.29 프로그래머스 레벨 1 : 1문제 (개인정보 수집 유효기간)
    map, 날짜 계산으로 코드 가독성 개선

--- 23.05/05.29/Pro_1/개인정보 수집 유효기간.py
+++ 23.05/05.29/Pro_1/개인정보 수집 유효기간.py
@@ -1,23 +1,22 @@
 # 16분 소요, 혼자서 풂 (단, datetime은 검색 함)
+# 2차 개선 : 그냥 총 date 합으로 계산해서 datetime 쓰지 말자 (개선이 은근 오래걸림..365가 아니라 28일임에 주의하자)
+# => map써서 코드 간단히 하자
 
 # 모든 달이 28일까지 있을 때, today 기준 파기해야 할 privacies의 개인정보 번호 반환
 # today는 YYYY.MM.DD / terms는 약관종류 (A-Z) 유효기간 (1-100)/ privacies는 날짜 (수집된 날짜) 약관종류
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
+
+def make_date(date_str):
+    year, month, date = map(int, date_str.split('.'))
+    return year*28*12 + month*28 + date
 
 
 def solution(today, terms, privacies):
-    answer, terms_dict = [], dict()
-
     # today, terms 기준 term 별로 파기될 privacy 수집된 날짜 계산
-    for term in terms:
-        term_type, term_month = term.split(' ')
-        terms_dict[term_type] = (datetime.strptime(today, '%Y.%m.%d') - relativedelta(months=int(term_month)))
+    answer, terms_dict = [], {term[0]: int(term[2:])*28 for term in terms}
+    today = make_date(today)
 
     for idx, privacy in enumerate(privacies):
         collected, term = privacy.split(' ')
-        collected_date = datetime(int(collected.split('.')[0]), int(collected.split('.')[1]),
-                                  int(collected.split('.')[2]))
-        if collected_date <= terms_dict[term]:
+        if make_date(collected) + terms_dict[term]<= today:
             answer.append(idx + 1)
     return answer
