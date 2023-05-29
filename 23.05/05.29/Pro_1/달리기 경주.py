5432aeb0e79dae1f41eff14cf6737633f6c0fa19
soeun lee <thdmsdl87@gmail.com>
2023-05-29 13:56:45 +0900

    [해결, 개선] 23.05.29 프로그래머스 레벨 1 : 1문제 (달리기 경주)

--- 23.05/05.29/Pro_1/달리기 경주.py
+++ 23.05/05.29/Pro_1/달리기 경주.py
@@ -1,19 +1,22 @@
-# 13:32 -
+# 22분 소요, 3차 성공 (다른 사람 풀이 참고)
 # 1차 시도 : 배열에 있는 원소로 다 swap하기 (정렬 문제) => 시간 초과
-# 2차 시도 : 이름 별 idx (hash 사용)
+# 2차 시도 : 이름 별 idx (hash 사용) => target name 찾는데서 시간 초과 (그래도 좀 줄었음)
+# 3차 시도 : hash, swap 같이 쓰기 => 성공 / 다른 사람 풀이 봄
 
 # 바로 앞 선수 추월 시, 추월한 사람 이름 부름
 # 현 순서 이름 배열, 이름 부른 배열 줄 때, 선수 등수 순 이름 배열 반환하기
-result = dict()
+rank_dict = dict()
 
 
 def solution(players, callings):
     for idx, player in enumerate(players):
-        result[player] = idx
+        rank_dict[player] = idx
 
     for calling in callings:
-        target = [name for name, rank in result.items() if rank == result[calling] - 1][0]
-        result[calling] -= 1
-        result[target] += 1
-    answer = dict(sorted(result.items(), key=lambda item: item[1]))
-    return list(answer.keys())
+        pre_player_rank, post_player_rank = rank_dict[calling] - 1, rank_dict[calling]
+        pre_player_name, post_player_name = players[pre_player_rank], players[post_player_rank]
+        # dict 갱신
+        rank_dict[pre_player_name], rank_dict[post_player_name] = post_player_rank, pre_player_rank
+        # list 갱신
+        players[pre_player_rank], players[post_player_rank] = players[post_player_rank], players[pre_player_rank]
+    return players
