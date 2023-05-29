08b090269d63cedb0f33406f43415fe15f5f99f6
soeun lee <thdmsdl87@gmail.com>
2023-05-29 17:11:10 +0900

    [개선] 23.05.29 프로그래머스 레벨 1: 1문제 (신고 결과 받기)
    index 함수 이용해서 보다 가독성있게 코드 개선

--- 23.05/05.29/Pro_1/신고 결과 받기.py
+++ 23.05/05.29/Pro_1/신고 결과 받기.py
@@ -1,4 +1,5 @@
 # 1차 성공: 13분 소요, 혼자 풂
+# 2차 개선: intersection 대신 index
 # 신고 횟수는 user 별로 1회씩만 처리됨 (여러번 신고해도 1번으로 처리)
 # k번 이상 신고되면 정지, 해당 유저를 신고한 모든 유저에게 메일 발송 => user별로 처리 결과 받은 메일 횟수 배열에 담아 return
 
@@ -8,21 +9,18 @@ def intersection(lst1, lst2):
 
 
 def solution(id_list, report, k):
-    # key: user value: user별로 신고한 상대 id 목록
-    user_report_dict = {id: set() for id in id_list}
+    answer = [0] * len(id_list)
     # key: user value: user별로 신고당한 횟수
     user_reported_dict = {id: 0 for id in id_list}
 
-    for row in report:
-        user, reported_user = row.split(' ')
-        user_report_dict[user].add(reported_user)
+    # user별로 신고당한 횟수 계산
+    for row in set(report):
+        user_reported_dict[row.split()[1]] += 1
 
-    for value in user_report_dict.values():
-        for id in value:
-            user_reported_dict[id] += 1
+    # 신고한 user별로 신고당한 횟수가 k 이상인 사람마다 +1
+    # index로 answer랑 id_list 매핑
+    for row in set(report):
+        if user_reported_dict[row.split()[1]] >= k:
+            answer[id_list.index(row.split()[0])] += 1
 
-    suspended_user = [id for id, count in user_reported_dict.items() if count >= k]
-    answer = [len(intersection(reported_list, suspended_user)) for id, reported_list in user_report_dict.items()]
     return answer
-
-
